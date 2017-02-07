1f50808fd2c1afbf60947bb856bed2f1ee65eb65
Nathan Sweeney <nsweeney@riversideresearch.org>
2017-02-07 16:01:08 -0600

    vue switch

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <vue-switch name="myswitch"></vue-switch>
+  </div>
+</template>
+
+<script>
+import VueSwitch from './components/VueSwitch'
+
+export default {
+  name: 'app',
+  components: {
+    VueSwitch
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/VueSwitch.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <p class="control has-addons">
+      <a v-for="btn in buttons" class="button" v-bind:class="{'is-active': (btn==picked)}" @click.prevent="picked=btn">
+        <span>{{btn}}</span>
+      </a>
+    </p>
+
+    <p class="control is-hidden">
+      <label class="radio" v-for="btn in buttons">
+        <input type="radio" :value="btn" v-model="picked" :name="name">
+      </label>
+    </p>
+  </div>
+  {{picked}}
+</template>
+
+<script>
+  export default {
+    name: 'vue-switch',
+    props: { name: {required:true} },
+    data() {
+      return {
+        picked: 'No',
+        buttons: ['Yes','No']
+      }
+    }
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
